a0edf3a79291781961943925377ced27ba52f6e2
uniquehp19-cb <uniquehp19@gmail.com>
2026-03-30 21:12:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
-from tensorflow.keras.models import load_model  # only for loading model
+from keras.models import load_model  # standalone keras
 from streamlit_drawable_canvas import st_canvas
 
 # Load pre-trained model
@@ -22,7 +22,7 @@ canvas_result = st_canvas(
 )
 
 if canvas_result.image_data is not None:
-    # Preprocess the drawn image
+    # Preprocess drawn image
     img = Image.fromarray(np.uint8(canvas_result.image_data[:, :, 0]))
     img = img.resize((28, 28)).convert('L')
     img_array = np.array(img)/255.0
